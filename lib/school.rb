c2969ab05ce76ba5f72f9971502e2befaeb355be
Duy Pham <ericpham19@outlook.com>
2022-09-20 10:35:22 +1000

    finished

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+    attr_reader :name
+
+    def initialize(name)
+        @name= name
+        @roster= {}
+
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student (name, grade)
+        @name= name
+        @grade= grade
+        (@roster[grade] ||= []) << name
+       
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster= {}
+        @roster.map do |grade, name|
+            sorted_roster[grade]= name.sort
+        end
+        sorted_roster
+    end
+
+end
